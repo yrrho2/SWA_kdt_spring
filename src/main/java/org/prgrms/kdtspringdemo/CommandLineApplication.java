64b7d3dba9a84f2d86c45150e4f571edad2d64d9
yrrho2 <dyrho22@gmail.com>
2023-07-05 17:51:46 +0900

    Mission_CommandLineApplication_1.0

---
+++ src/main/java/org/prgrms/kdtspringdemo/CommandLineApplication.java
@@ -0,0 +1,10 @@
+package org.prgrms.kdtspringdemo;
+
+public class CommandLineApplication {
+    public static void main(String[] args) {
+        //여기서, 미션2 수행.
+        // command 3가지를 만들어야함/
+        // exit, create, list.
+        // voucher를 만들고 리스트 만드는 것. 내일아침에 하면 될듯. 재밌겠다.
+    }
+}
