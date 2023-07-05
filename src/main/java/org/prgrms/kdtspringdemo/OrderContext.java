435fa56e2314e2d844d74c21ff49628d8741a03d
yrrho2 <dyrho22@gmail.com>
2023-07-05 16:55:33 +0900

    OrderContext >> IoC Container

--- src/main/java/org/prgrms/kdtspringdemo/OrderContext.java
+++ src/main/java/org/prgrms/kdtspringdemo/OrderContext.java
@@ -30,3 +30,16 @@ public class OrderContext {
     // OrderService가 어떤 voucerService와 어떤 orderRepository를 쓰는지
     // 관계에 대한 책임도 가지고있다.
 }
+/*
+ OrderContext가 여러 객체들간의 관계를 맺게 해줌.
+ 객체가 자신이 사용할 객체를 직접 생성하지 않게 됨.
+
+ OrderService가 사용할 객체들을 OrderContext가 생성하게 되었음.
+ 이것이 IoC가 이루어 졌다고 보고, 생성과 관계에 대한 책임, 권한이 있는것을
+ IoC컨테이너라고 부름. 여기서는 OrderContext가 그럼.
+
+ 프레임워크나, Context, 콘테이너들에게 객체 생성이나 관계 대한 권한을 위임해버리면
+ 단단한 결합도가 생성되지 않음.
+
+
+ */
