18ca0478448862bf06a4ca844fb04726c37c2af8
yrrho2 <dyrho22@gmail.com>
2023-07-06 14:33:45 +0900

    CircularDependency Example

---
+++ src/main/java/org/prgrms/kdtspringdemo/CircularDepTester.java
@@ -0,0 +1,34 @@
+package org.prgrms.kdtspringdemo;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+class A{
+    private final B b;
+    A(B b){
+        this.b=b;
+    }
+}
+class B{
+    private final A a;
+    public B(A a) {
+        this.a = a;
+    }
+}
+
+@Configuration
+class CircularConfig{
+    @Bean
+    public A a(B b){
+        return  new A(b);
+    }
+    @Bean B b(A a){
+        return new B(a);
+    }
+}
+public class CircularDepTester {
+    public static void main(String[] args) {
+        new AnnotationConfigApplicationContext(CircularConfig.class);
+    }
+}
