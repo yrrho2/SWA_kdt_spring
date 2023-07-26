cbe9cffd5db7ae0a81a3d30568a4d2d16b96ddc4
yrrho2 <dyrho22@gmail.com>
2023-07-26 16:49:41 +0900

    Log Color.

--- src/main/java/org/prgrms/kdtspringdemo/OrderTester.java
+++ src/main/java/org/prgrms/kdtspringdemo/OrderTester.java
@@ -7,6 +7,7 @@ import org.prgrms.kdtspringdemo.voucher.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.BeanFactoryAnnotationUtils;
+import org.springframework.boot.ansi.AnsiOutput;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.util.Assert;
 
@@ -25,6 +26,7 @@ import java.util.stream.Collectors;
 public class OrderTester {
             private static final Logger logger = LoggerFactory.getLogger(OrderTester.class);
     public static void main(String[] args) throws IOException {
+        AnsiOutput.setEnabled(AnsiOutput.Enabled.ALWAYS);
         var applicationContext =  new AnnotationConfigApplicationContext(AppConfiguration.class);
         applicationContext.register(AppConfiguration.class);
         var environment = applicationContext.getEnvironment();
@@ -67,11 +69,11 @@ public class OrderTester {
         var orderService = applicationContext.getBean(OrderService.class);
 
 
-        logger.info(MessageFormat.format("Resource -> {0}", resource.getClass().getCanonicalName()));
-        logger.info(strings.stream().reduce("",(a,b)->a+"\n"+b));
+        logger.error(MessageFormat.format("Resource -> {0}", resource.getClass().getCanonicalName()));
+        logger.warn(strings.stream().reduce("",(a,b)->a+"\n"+b));
+        logger.warn(MessageFormat.format("is Jdbc Repo -> {0}", voucherRepository instanceof JdbcVoucherRepository));
+        logger.warn(MessageFormat.format("is Jdbc Repo -> {0}", voucherRepository.getClass().getCanonicalName()));
         //logger.info(contents);
-        logger.info(MessageFormat.format("is Jdbc Repo -> {0}", voucherRepository instanceof JdbcVoucherRepository));
-        logger.info(MessageFormat.format("is Jdbc Repo -> {0}", voucherRepository.getClass().getCanonicalName()));
 
         var order = orderService.createOrder(customerId, new ArrayList<OrderItem>(){{
             add(new OrderItem(UUID.randomUUID(), 100L,1));
